cbcf5324ce50ee8c377da1365103347193a9f80d
samchat2561 <samchat2561@gmail.com>
2025-12-12 16:26:34 +0700

    update laravel12-menu

---
+++ resources/views/livewire/partials/navbar.blade.php
@@ -0,0 +1,28 @@
+<!-- Header -->
+<header class="bg-white shadow-sm">
+    <nav class="container mx-auto px-4 py-4">
+        <div class="flex items-center justify-between">
+            <!-- Logo -->
+            <div class="text-2xl font-bold text-gray-800"> <a href="/">My Blog</a></div>
+            <!-- Navigation -->
+            <ul class="flex space-x-8">
+
+                <li>
+                    <button class="text-gray-600 hover:text-gray-900 flex items-center focus:outline-none">
+
+                    </button>
+
+                    <ul x-show="open" x-transition
+                        class="absolute left-0 mt-2 w-48 bg-white shadow-lg rounded-md z-10 py-1">
+
+                        <li><a href="" class="block px-4 py-2 text-gray-600 hover:bg-gray-100"></a>
+                        </li>
+
+                    </ul>
+                </li>
+
+
+            </ul>
+        </div>
+    </nav>
+</header>
